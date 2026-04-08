7c2101da008d0d88322330c7e17179e7643f0142
sujith-mg <sujith7134@gmail.com>
2026-04-08 23:40:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (87.29%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,11 +1,5 @@
 class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        
+    def search(self, nums, target):    
         left, right = 0, len(nums) - 1
         
         while left <= right:

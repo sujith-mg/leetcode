aeeffb3a87a2a1384d92849fab7f668a44bac2c7
sujith-mg <sujith7134@gmail.com>
2026-04-22 19:52:57 +0530

    Time: 909 ms (51.35%) | Memory: 13 MB (41.17%) - LeetSync

---
+++ 322-coin-change/coin-change.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        # dp[i] = minimum coins needed for amount i
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+        
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], 1 + dp[i - coin])
+        
+        return dp[amount] if dp[amount] != float('inf') else -1
